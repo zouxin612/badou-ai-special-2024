8f9d649eabb6f63755f35f4b4fc04fcde151864b
lys66 <lys20020101@163.com>
2024-03-25 20:38:14 +0800

    第二周代码

--- test.py
+++ test.py
@@ -1,15 +1,6 @@
-import datetime
-import time
+import torch
+from torchvision import models
+from torchvision.models.densenet import DenseNet121_Weights
 
-def remind_me():
-    while True:
-        # 获取当前日期和时间
-        current_date = datetime.datetime.now()
-        # 检查是否是周日并且当前时间是9点
-        if current_date.weekday() == 6 and current_date.hour == 9:
-            print("现在是上午9点，今天是周日，记得上课！")
-        # 每小时检查一次
-        time.sleep(3600)
+a = models.densenet121(weights=DenseNet121_Weights.IMAGENET1K_V1)
 
-
-remind_me()
