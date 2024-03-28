623d83c79d27495af12bf0870a00bb16b1bfc799
zhushijie <774328066@qq.com>
2024-03-28 19:51:49 +0800

    实现最邻近插值

--- 95-朱世杰/nearset_insert.py
+++ 95-朱世杰/nearset_insert.py
@@ -14,14 +14,14 @@ def nearestInsertValue(img, height, width):
     new_img = np.zeros([height, width, c], img.dtype)
     for i in range(height):
         for j in range(width):
-            row = round(i / h_ratio)
-            col = round(j / w_ratio)
+            row = min(round(i / h_ratio), old_height - 1)
+            col = min(round(j / w_ratio), old_width - 1)
             new_img[i][j] = img[row][col]
     return new_img
 
 
 img = cv2.imread('lenna.png')
-new_img = nearestInsertValue(img, 800, 800)
+new_img = nearestInsertValue(img, 1200, 800)
 cv2.imshow('old img', img)
 cv2.imshow('new img', new_img)
 cv2.waitKey(0)
