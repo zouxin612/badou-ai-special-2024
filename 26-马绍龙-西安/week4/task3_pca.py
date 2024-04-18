f25deac1d8351f73dbb203381840208938a4d280
ma_shaolong <ma_shaolong@topsec.com.cn>
2024-04-18 10:54:46 +0800

    修改注释

--- 26-马绍龙-西安/week4/task3_pca.py
+++ 26-马绍龙-西安/week4/task3_pca.py
@@ -1,14 +1,12 @@
 import numpy as np
 
-
+'''用PCA求样本矩阵X    计算出K阶降维后的矩阵Z
+Note:请保证输入的样本矩阵X shape=(m, n)，m行样例，n个特征
+'''
 class CPCA(object):
-    '''用PCA求样本矩阵X    计算出K阶降维后的矩阵Z
-    Note:请保证输入的样本矩阵X shape=(m, n)，m行样例，n个特征
-    '''
-
-    def __init__(self, X, K):  # X,样本矩阵X      K 为X要特征降维成k阶
+    def __init__(self, X, K):  # 初始化方法, X为样本矩阵X ,K为X要降维成k阶
         self.X = X  # 样本矩阵X
-        self.K = K  # K阶降维矩阵的K值
+        self.K = K  # 保留的维度数
         self.centrX = []  # 矩阵X的中心化后的矩阵
         self.C = []  # 样本集的协方差矩阵C
         self.U = []  # 特征向量矩阵
@@ -25,11 +23,11 @@ class CPCA(object):
         print('样本集的特征均值:\n', mean)
         centrX = self.X - mean  # 样本集的中心化
         print('样本矩阵X的中心化centrX:\n', centrX)
-        return centrX  # 新生成的NumPy数组将是一个长度为n 的一维数组，其中每个元素对应原数组 self.X 的一列的平均值。
+        return centrX
 
     def _cov(self):  # 求样本矩阵X的协方差矩阵C
-        ns = np.shape(self.centrX)[0]  # 原始特征的行数
-        C = np.dot(self.centrX.T, self.centrX) / (ns - 1)  # 样本矩阵的协方差矩阵C
+        ns = np.shape(self.centrX)[0]     # 原始特征的行数
+        C = np.dot(self.centrX.T, self.centrX) / (ns - 1)  # 样本矩阵的协方差矩阵C,这里矩阵乘的时候，转置矩阵一定要放在前面，这样结果则是输入矩阵的特征列数的方阵。
         print('样本矩阵X的协方差矩阵C:\n', C)
         return C
 
@@ -39,8 +37,8 @@ class CPCA(object):
         print('样本集的协方差矩阵C的特征值:\n', a)
         print('样本集的协方差矩阵C的特征向量:\n', b)
 
-        ind = np.argsort(-1 * a)  # 给出特征值降序的topK的索引序列
-        UT = [b[:, ind[i]] for i in range(self.K)]  # 构建K阶降维的降维转换矩阵U
+        ind = np.argsort(-1 * a)  # 给出特征值降序的topK的  索引序列
+        UT = [b[:, ind[i]] for i in range(self.K)]  # 构建K阶降维的降维转换矩阵UT
         U = np.transpose(UT)
         print('%d阶降维转换矩阵U:\n' % self.K, U)
         return U
